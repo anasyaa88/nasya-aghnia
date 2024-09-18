61965fafe548741b19a731162bcd3bdb2aff6fbf
anasyaa88 <nasyapribadi248@gmail.com>
2024-09-18 13:36:22 +0700

    Add files via upload

---
+++ JOptionPane.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Hype GLK
+ */
+class JOptionPane {
+
+    static void showMessageDialog(Object object, String saya_Belajar_NetBeans_) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
